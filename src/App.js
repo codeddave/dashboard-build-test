fa4265c2cf56af5e3a48379f9abd3deb0640dfb0
codeddave <drizzzydave@gmail.com>
2022-02-11 13:20:38 +0100

    starter files, tailwindcss set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-red-600 text-blue-500">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
