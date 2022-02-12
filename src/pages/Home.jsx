827562b1f456f04fa646e57be86091fa5957be6f
codeddave <drizzzydave@gmail.com>
2022-02-12 15:43:44 +0100

    home page build

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainContent from "./MainContent.jsx";
+import SideNav from "./SideNav.jsx";
+
+const Home = () => {
+  return (
+    <div>
+      <SideNav />
+      <MainContent />
+    </div>
+  );
+};
+
+export default Home;
