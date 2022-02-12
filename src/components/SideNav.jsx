0039c1ecfa3817d3a0096355f4f69e1a1a2361bf
codeddave <drizzzydave@gmail.com>
2022-02-12 22:00:16 +0100

    updates to the side naav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
+const SideNavWrapper = styled.aside`
+  width: 15vh;
+`;
 const SideNav = () => {
-  return <div></div>;
+  return (
+    <SideNavWrapper className="app-dark h-screen">
+      <div className="flex justify-center flex-col items-center pt-8">
+        <div className="w-12 h-12 p-2 rounded-full flex justify-center items-center bg-white">
+          <img src="" alt="" />
+        </div>
+
+        <p className="text-white text-xs font-semibold"> ADD A USER</p>
+      </div>
+    </SideNavWrapper>
+  );
 };
 
 export default SideNav;
