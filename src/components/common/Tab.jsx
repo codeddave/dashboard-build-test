cdd58ebbfe52f6bf5d1324f369a002e90f084dcb
codeddave <drizzzydave@gmail.com>
2022-02-13 20:35:25 +0100

    updates to tab components styling

--- src/components/common/Tab.jsx
+++ src/components/common/Tab.jsx
@@ -12,14 +12,16 @@ export const Tab = ({
     <div
       onClick={disabled ? undefined : onClick}
       className={classnames(
-        `sm:w-${width} py-2  px-3 sm:px-0 text-center font-medium cursor-pointer border-b-2 text-white text-sm md:text-base leading-snug`,
+        `sm:w-${width} py-2  px-3 sm:px-0 text-center font-medium cursor-pointer border-b-2  text-sm md:text-base leading-snug mr-6 `,
         {
-          "border-blue-400 text-blue-400 font-extrabold  focus:text-blue-800 focus:border-blue-700":
+          "border-blue-400 text-blue-400 font-bold  focus:text-blue-800 focus:border-blue-700":
             !disabled && current,
-          "border-transparent  hover:text-gray-300 hover:font-bold focus:outline-none":
+          "border-transparent  hover:text-gray-500 hover:font-bold focus:outline-none":
             !disabled && !current,
           "border-transparent text-gray-500 focus:outline-none italic":
             disabled,
+
+          "text-gray-500": !current,
         }
       )}
     >
