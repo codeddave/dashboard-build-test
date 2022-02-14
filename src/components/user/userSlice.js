11db91dfb3463bb0650dfdb011f1356ff0e3e102
codeddave <drizzzydave@gmail.com>
2022-02-14 23:47:50 +0100

    bulding userslice

---
+++ src/components/user/userSlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { userApi } from "../../api/users";
+
+const getUsers = createAsyncThunk(
+  "users/fetchByIdStatus",
+  async (userId, thunkAPI) => {
+    const response = await userApi.getUsers();
+    return response;
+  }
+);
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    users: [],
+    isLoading: "",
+    error: null,
+  },
+  reducers: {
+    getUsers: (state, action) => {},
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.isLoading = "resolved";
+        state.users = action.payload;
+      })
+      .addCase(getUsers.pending, (state) => {
+        state.isLoading = "pending";
+      })
+      .addCase(getUsers.rejected, (state, action) => {
+        state.isLoading = "rejected";
+        state.error = action.error;
+      });
+  },
+});
+
+export const { reducer: userReducer } = userSlice;
