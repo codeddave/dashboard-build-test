e3ad1cd9e87834eb653e7b4960e4feb565b8f9d7
codeddave <drizzzydave@gmail.com>
2022-02-15 22:44:43 +0100

    updates

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,5 +1,6 @@
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import CustomFormInput from "./form/CustomFormInput";
 
 const AddUser = () => {
   return (
@@ -10,17 +11,30 @@ const AddUser = () => {
         left: "50%",
         transform: "translate(-50%, -50%)",
       }}
-      className="modal-bg-color bg-white border bg-blue shadow-xl p-6 transform transition-all w-full md:max-w-2xl opacity-100  overflow-x-hidden overflw-y-scroll fixed z-50 tracking-wider  scroll "
+      className="modal-bg-color text-gray-600 bg-white border bg-blue shadow-xl p-6 transform transition-all w-full md:max-w-2xl opacity-100  overflow-x-hidden overflw-y-scroll fixed z-50 tracking-wider  scroll "
     >
-      <p className="text-4xl font-bold">ADD USER</p>
-      <p className="text-2xl font-bold pt-2.5">1/2 Personal Details</p>
-      <Formik>
-        {() => (
-          <Form>
-            <Field />
-          </Form>
-        )}
-      </Formik>
+      <section className="shadow h-full p-6">
+        <p className="text-4xl font-bold">ADD USER</p>
+        <p className="text-2xl font-bold pt-2.5 pb-5">1/2 Personal Details</p>
+        <Formik>
+          {() => (
+            <Form>
+              <Field
+                component={CustomFormInput}
+                placeholder="Title"
+                name="title"
+                title="Title"
+              />
+              <Field
+                component={CustomFormInput}
+                placeholder="First Name"
+                name="firstName"
+                title="First name"
+              />
+            </Form>
+          )}
+        </Formik>
+      </section>
     </section>
   );
 };
