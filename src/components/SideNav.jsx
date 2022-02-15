7e20a90bac0a6d21ca74e28025000fdc56ca7dbb
codeddave <drizzzydave@gmail.com>
2022-02-15 22:46:03 +0100

    handled add user modal state

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
 import styled from "styled-components";
 import AddUserIcon from "../assets/icons/add-user.svg";
+import AddUser from "./AddUser";
 const SideNavWrapper = styled.aside`
   width: 15vh;
 `;
 const SideNav = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <SideNavWrapper className="app-dark h-screen">
-      <div className="flex justify-center flex-col items-center pt-8">
+      <div
+        className="flex justify-center flex-col items-center pt-8"
+        onClick={() => setIsModalOpen(true)}
+      >
         <div className="w-12 h-12 p-2 rounded-full flex justify-center items-center bg-white">
           <img src={AddUserIcon} alt="add-user" />
         </div>
 
         <p className="text-white text-xs font-semibold pt-2"> ADD A USER</p>
       </div>
+
+      {isModalOpen ? (
+        <ClickAwayListener onClickAway={closeModal}>
+          <AddUser />
+        </ClickAwayListener>
+      ) : null}
     </SideNavWrapper>
   );
 };
