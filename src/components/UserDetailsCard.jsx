a0ee60724038e84cdf565257dc414697865355cc
codeddave <drizzzydave@gmail.com>
2022-02-15 11:22:49 +0100

    updates to userDetails card

--- src/components/UserDetailsCard.jsx
+++ src/components/UserDetailsCard.jsx
@@ -1,13 +1,46 @@
 import React from "react";
-import Person from "../../assets/images/big-person.svg";
 
-const UserDetailsCard = () => {
+const UserDetailsCard = ({ user }) => {
   return (
-    <div>
-      <div className="grid grid-cols-2 gap-x-4">
-        <img src={Person} alt="person" />
-      </div>
-    </div>
+    <section className="border-2 rounded h-96 w-1/4 mr p-4 text-xs">
+      {user.picture ? (
+        <div>
+          <div className="grid grid-cols-2 gap-x-3">
+            <img
+              src={user?.picture}
+              alt="person"
+              className="w-32 h-32 rounded object-cover"
+            />
+
+            <div className="">
+              <div className="pb-2">
+                <p className="text-gray-500 pb-1 extra-small-font ">
+                  FULL NAME
+                </p>
+                <p className="small-font">
+                  {user.title} {user.firstName} {user.lastName}
+                </p>
+              </div>
+
+              <div className="pb-2">
+                <p className="text-gray-400 pb-1 extra-small-font ">
+                  EMAIL ADDRESS
+                </p>
+                <p className="break-words small-font ">{user.email}</p>
+              </div>
+              <div className="pb-2">
+                <p className="text-gray-400 pb-1 extra-small-font ">
+                  PHONE NUMBER
+                </p>
+                <p className="small-font">{user.phone}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p>Select a User</p>
+      )}
+    </section>
   );
 };
 
