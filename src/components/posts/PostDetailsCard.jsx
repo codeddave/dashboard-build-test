4e5c6960274a1395fc1ea0af31ee116f0e08822a
codeddave <drizzzydave@gmail.com>
2022-02-17 21:17:14 +0100

    updated Post details component

--- src/components/posts/PostDetailsCard.jsx
+++ src/components/posts/PostDetailsCard.jsx
@@ -2,15 +2,22 @@ import React from "react";
 
 const PostDetailsCard = ({ post }) => {
   console.log(post);
+  if (!post) return <p>hel</p>;
   return (
     <section className="border-2 rounded h-96 w-1/4 mr p-4 text-xs">
-      <img
-        src={post?.image}
-        alt="post"
-        className="w-full h-32 object-cover rounded"
-      />
+      {post.image ? (
+        <section>
+          <img
+            src={post?.image}
+            alt="post"
+            className="w-full h-32 object-cover rounded"
+          />
 
-      <p className="pt-2">{post.text} </p>
+          <p className="pt-2">{post.text} </p>
+        </section>
+      ) : (
+        <p className="text-center pt-20">Please Select a post from list </p>
+      )}
     </section>
   );
 };
--- src/components/user/userSelectors.js
+++ src/components/user/userSelectors.js
@@ -6,3 +6,7 @@ const getUser = (state) => {
 
 export const selectUsers = createSelector(getUser, (user) => user.users);
 export const selectUser = createSelector(getUser, (user) => user.user);
+export const selectUsersLoading = createSelector(
+  getUser,
+  (user) => user.isLoading
+);
