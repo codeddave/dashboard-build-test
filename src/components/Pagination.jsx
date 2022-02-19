89cb9c59d51df9e17b56ac23e4b2f571646104ec
codeddave <drizzzydave@gmail.com>
2022-02-19 23:46:18 +0100

    updates

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,23 @@
-const Pagination = ({ dataPerPage, data, handlePageNumberCLick }) => {
+const Pagination = ({
+  dataPerPage,
+  data,
+  handlePageNumberCLick,
+  handleNextClick,
+  handlePrevClick,
+}) => {
   console.log(data);
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(data?.data?.length / dataPerPage); i++) {
     pageNumbers.push(i);
   }
   console.log(pageNumbers);
+  if (!pageNumbers.length) return null;
   return (
-    <nav className="b text-white mb-10 px-4 border w-1/4 shadow h-12 mt-2  flex flex-col justify-center ml-12">
+    <nav className="b text-white mb-10 px-4 border rounded w-1/4 shadow h-12 mt-2  flex flex-col justify-center ml-12">
       <div className="text-blue-400 flex items-center font-bold">
-        <button className="pr-4">Prev</button>
+        <button onClick={handlePrevClick} className="pr-4">
+          Prev
+        </button>
         <ul className="paginatin flex">
           {pageNumbers.map((number) => (
             <div className=" border p px-4 mr-3 rounded shadow-sm">
@@ -23,7 +32,12 @@ const Pagination = ({ dataPerPage, data, handlePageNumberCLick }) => {
             </div>
           ))}
         </ul>
-        <button className="pl-4 border px-4 rounded py-0.5">Next</button>
+        <button
+          onClick={handleNextClick}
+          className="pl-4 border px-4 rounded py-0.5"
+        >
+          Next
+        </button>
       </div>
     </nav>
   );
