4797843eb7eea0abe07dd953bf95d312794cb041
codeddave <drizzzydave@gmail.com>
2022-02-19 21:08:59 +0100

    fixing Pagination issuesm

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,13 @@
-const Pagination = ({ dataPerPage, totalData, handlePageNumberCLick }) => {
+const Pagination = ({ dataPerPage, data, handlePageNumberCLick }) => {
+  console.log(data);
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalData / dataPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(data?.data?.length / dataPerPage); i++) {
     pageNumbers.push(i);
   }
-
+  console.log(pageNumbers);
   return (
-    <nav className="d-flex justify-content-center">
+    <nav className="d-flex justify-content-center bg-black text-white">
+      <p>afbwfkbn wbkrkbk</p>
       <ul className="pagination">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
