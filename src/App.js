84df788c1ff03c24b3f6c14ef4fe62217a08434a
codeddave <drizzzydave@gmail.com>
2022-02-20 02:13:19 +0100

    added toast to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import Home from "./pages/Home";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <div className="App  ">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      {/* Same as */}
       <Home />
     </div>
   );
