d76974ae4353f6d898f162d505619867ac5ff99c
codeddave <drizzzydave@gmail.com>
2022-02-20 02:21:36 +0100

    api fns updates, wrapped api calls in trycatch blocks

--- src/api/users.js
+++ src/api/users.js
@@ -1,26 +1,58 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export const getUsers = async () => {
-  const config = {
-    headers: {
-      "app-id": process.env.REACT_APP_API_KEY,
-    },
-  };
-  const res = await axios.get("https://dummyapi.io/data/v1/user", config);
-  return res.data;
+  try {
+    const config = {
+      headers: {
+        "app-id": process.env.REACT_APP_API_KEY,
+      },
+    };
+    const res = await axios.get("https://dummyapi.io/data/v1/user", config);
+    return res.data;
+  } catch (error) {
+    toast.error(error);
+  }
 };
 
 export const getUserbyId = async (id) => {
+  try {
+    const config = {
+      headers: {
+        "app-id": process.env.REACT_APP_API_KEY,
+      },
+    };
+    const res = await axios.get(
+      `https://dummyapi.io/data/v1/user/${id}`,
+      config
+    );
+    return res.data;
+  } catch (error) {
+    toast.error(error);
+  }
+};
+
+export const createUser = async (userData) => {
   const config = {
     headers: {
       "app-id": process.env.REACT_APP_API_KEY,
     },
   };
-  const res = await axios.get(`https://dummyapi.io/data/v1/user/${id}`, config);
-  return res.data;
+  try {
+    const res = await axios.post(
+      "https://dummyapi.io/data/v1/user/create",
+      userData,
+      config
+    );
+    toast.success("User created successfully");
+    return res.data;
+  } catch (error) {
+    toast.error(error);
+  }
 };
 
 export const userApi = {
   getUsers,
   getUserbyId,
+  createUser,
 };
